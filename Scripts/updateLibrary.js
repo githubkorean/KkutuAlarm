77d229a027cbb8378c1ba3d75a6f336bb9a72e2a
githubkorean <100678096+githubkorean@users.noreply.github.com>
2024-10-21 15:51:50 +0900

    Update updateLibrary.js

--- Scripts/updateLibrary.js
+++ Scripts/updateLibrary.js
@@ -1,50 +1,50 @@
 // updateLibrary.js
-(function() {
-    // 페이지가 로드된 후 실행
-    window.addEventListener('load', function() {
-        const currentVersion = GM_info.script.version;  // 현재 버전 가져오기
+function checkForUpdates(repoURL, currentVersion) {
+    const versionURL = `https://raw.github.com/githubkorean/KkutuAlarm/master/Version.txt`;  // 깃허브에 저장한 버전 정보 파일의 URL
+    const scriptBaseURL = `https://raw.github.com/githubkorean/KkutuAlarm/master/Scripts/`;  // 깃허브 스크립트 파일의 기본 URL
 
-        // 마지막 업데이트 체크 시간을 가져오기
-        const lastCheck = GM_getValue('lastCheck', 0);
-        const now = Date.now();
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: versionURL,
+        onload: function(response) {
+            const latestVersion = response.responseText.trim();
 
-        // 24시간이 지난 경우에만 업데이트 확인
-        if (now - lastCheck > 24 * 60 * 60 * 1000) {
-            checkForUpdates('https://github.com/githubkorean/KkutuAlarm', currentVersion);
+            if (latestVersion !== currentVersion) {
+                console.log(`New version ${latestVersion} available. Checking user response...`);
+                showUpdatePrompt(latestVersion, scriptBaseURL);
+            } else {
+                console.log('You are using the latest version.');
+            }
         }
     });
+}
 
-    function checkForUpdates(repoURL, currentVersion) {
-        const repo = repoURL.replace(/https:\/\/github.com\//, '');
-        const versionURL = `https://raw.githubusercontent.com/${repo}/master/Version.txt`;
-        const scriptBaseURL = `https://raw.githubusercontent.com/${repo}/master/Scripts/`;
+function showUpdatePrompt(latestVersion, scriptBaseURL) {
+    const message = `New version ${latestVersion} available. Would you like to update now?`;
+    const updateNow = confirm(message);
 
-        GM_xmlhttpRequest({
-            method: 'GET',
-            url: versionURL,
-            onload: function(response) {
-                const latestVersion = response.responseText.trim();
-
-                if (latestVersion !== currentVersion) {
-                    console.log(`New version ${latestVersion} available. Updating...`);
-                    const userResponse = confirm(`New version ${latestVersion} available. Do you want to update?`);
-
-                    if (userResponse) {
-                        redirectToUpdate(latestVersion, scriptBaseURL);
-                    } else {
-                        GM_setValue('lastCheck', Date.now()); // 마지막 체크 시간을 기록
-                    }
-                } else {
-                    console.log('You are using the latest version.');
-                    alert('You are using the latest version.');
-                    GM_setValue('lastCheck', Date.now()); // 마지막 체크 시간을 기록
-                }
+    if (updateNow) {
+        redirectToUpdate(latestVersion, scriptBaseURL);
+    } else {
+        const postponeResponse = confirm("Would you like to be reminded tomorrow?");
+        if (postponeResponse) {
+            const tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            GM_setValue('nextUpdateReminder', tomorrow.getTime());
+            alert('You will be reminded tomorrow.');
+        } else {
+            const remindLater = confirm("Would you like to be reminded in 10 minutes?");
+            if (remindLater) {
+                const tenMinutesLater = new Date();
+                tenMinutesLater.setMinutes(tenMinutesLater.getMinutes() + 10);
+                GM_setValue('nextUpdateReminder', tenMinutesLater.getTime());
+                alert('You will be reminded in 10 minutes.');
             }
-        });
+        }
     }
+}
 
-    function redirectToUpdate(latestVersion, scriptBaseURL) {
-        const scriptURL = `${scriptBaseURL}${latestVersion}.user.js`;
-        GM_openInTab(scriptURL, { active: true, insert: true });
-    }
-})();
+function redirectToUpdate(latestVersion, scriptBaseURL) {
+    const scriptURL = `${scriptBaseURL}${latestVersion}.user.js`;
+    GM_openInTab(scriptURL, { active: true, insert: true });
+}
