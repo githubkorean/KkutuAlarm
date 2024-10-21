1b6becb544a18873d4e012d8ad46e2bd8c020035
githubkorean <100678096+githubkorean@users.noreply.github.com>
2024-10-21 14:52:06 +0900

    Update updateLibrary.js

--- Scripts/updateLibrary.js
+++ Scripts/updateLibrary.js
@@ -1,10 +1,5 @@
 // updateLibrary.js
-function getCurrentVersion() {
-    return GM_info.script.version;  // 메타 데이터에서 현재 버전 가져오기
-}
-
-function checkForUpdates(repoURL) {
-    const currentVersion = getCurrentVersion();  // 현재 버전 가져오기
+function checkForUpdates(repoURL, currentVersion) {
     const repo = repoURL.replace(/https:\/\/github.com\//, '');  // URL에서 repo 이름 추출
     const versionURL = `https://raw.githubusercontent.com/${repo}/main/Version.txt`;  // 깃허브에 저장한 버전 정보 파일의 URL
     const scriptBaseURL = `https://raw.githubusercontent.com/${repo}/main/Scripts/`;  // 깃허브 스크립트 파일의 기본 URL
