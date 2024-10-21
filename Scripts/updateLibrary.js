73bd90954b94b85c52d9f24d9f56b4f4760a51ba
githubkorean <100678096+githubkorean@users.noreply.github.com>
2024-10-21 14:44:35 +0900

    Create updateLibrary.js

---
+++ Scripts/updateLibrary.js
@@ -0,0 +1,28 @@
+// updateLibrary.js
+function checkForUpdates(repoURL, currentVersion) {
+    const repo = repoURL.replace(/https:\/\/github.com\//, '');  // URL에서 repo 이름 추출
+    const versionURL = `https://raw.githubusercontent.com/${repo}/main/Version.txt`;  // 깃허브에 저장한 버전 정보 파일의 URL
+    const scriptBaseURL = `https://raw.githubusercontent.com/${repo}/main/Scripts/`;  // 깃허브 스크립트 파일의 기본 URL
+
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: versionURL,
+        onload: function(response) {
+            const latestVersion = response.responseText.trim();
+
+            if (latestVersion !== currentVersion) {
+                console.log(`New version ${latestVersion} available. Updating...`);
+                alert(`New version ${latestVersion} available. Redirecting to update page...`);
+                redirectToUpdate(latestVersion, scriptBaseURL);
+            } else {
+                console.log('You are using the latest version.');
+                alert('You are using the latest version.');
+            }
+        }
+    });
+}
+
+function redirectToUpdate(latestVersion, scriptBaseURL) {
+    const scriptURL = `${scriptBaseURL}${latestVersion}.user.js`;
+    GM_openInTab(scriptURL, { active: true, insert: true });
+}
